ab7f3735b581cb5d16053f2555ec2390a6dc419b
Petri <petritrebs@me.com>
2020-08-12 22:35:34 -0400

    Made some progress

--- injected.js
+++ injected.js
@@ -1,9 +1,12 @@
 var page = document.location.pathname.toLowerCase();
 window.teamName = null;
+window.teamMembers = null;
 
 window.showFilters = showFilters;
 window.copyToClipboard = copyToClipboard;
 window.startRefreshTimer = startRefreshTimer;
+window.inc_loadFilter = inc_loadFilter;
+
 
 switch (page){
 	case "/login_locate_sso.do":
@@ -52,7 +55,10 @@ function buildLoginPage(){
 function makeResolverGreatAgain(){
 		document.getElementById('lnkResolver').innerHTML = "Resolver&#10133;";
         document.getElementById('search_filter_title').parentElement.innerHTML += "<input type=\"button\" style=\"display: none; margin-right: 10px;\" name=\"addFilterButton\" value=\"Add Filter\" onclick=\"$('#addFilterWindow').data('kendoWindow').open();\" id=\"addFilterButton\" class=\"k-button\"><input type=\"button\" name=\"showFilterButton\" value=\"Show Filters\" onclick=\"showFilters();\" id=\"showFilterButton\" class=\"k-button\"><input type=\"button\" style=\"margin-left: 5px;\" name=\"refreshButton\" value=\"Refresh\" onclick=\"refresh();\" id=\"refreshButton\" class=\"k-button\"><input id=\"enableRefresh\" type=\"checkbox\"><label for=\"enableRefresh\">Auto-refresh values every </label><input id=\"refreshMinutes\" type=\"number\" min=\"3\" value=\"5\"><label for=\"refreshMinutes\"> minutes.</label> <input type=\"button\" onclick=\"startRefreshTimer();\" value=\"Submit\">";
+        $('#panelbar-leftnav').append('<li id="liMyTeam" class="k-item k-state-default" role="menuitem"><span class="k-link k-header">My Team</span><ul id="inc_leftnav_savedFilters" class="leftnavSavedFilters leftnavSavedTeam k-group k-panel" role="group"></li>')
+
 		getTeamName();
+		getTeamMembers();
         ticketCount();
         document.getElementById('lblIncident').outerHTML += "<input type=\"button\" style=\"margin-left: 5px;\" name=\"copyTicketButton\" value=\"Copy\" onclick=\"copyToClipboard(IncidentNumber);\" id=\"copyTicketButton\" class=\"Button\">";
 }
@@ -71,9 +77,52 @@ function getTeamName(){
         },
         success: function(data){
            teamName = data.d.Data.Items[0].assignment_group;
+		   AskInfo('A|'+data.d.Data.Items[0].number);
         }
     });
 }
+
+function getTeamMembers(){
+	var requestBody = "{\"company\":\""+COMPANY+"\",\"take\":10,\"skip\":0,\"page\":1,\"pageSize\":10,\"sort\":[{\"field\":\"user.last_name\",\"dir\":\"asc\"}],\"filter\":{\"logic\":\"and\",\"filters\":[{\"field\":\"group.name\",\"operator\":\"=\",\"value\":\""+teamName+"\",\"logic\":\"\"},{\"field\":\"user\",\"operator\":\"LIKE\",\"value\":\"\",\"logic\":\"\"}]}}";
+	
+	$.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        async: true,
+        data: requestBody,
+        url: "https://support.compucom.com/SNDataEntities/SNGroupMember/WebMethods/SNGroupMemberWS.asmx/getGroupMember",
+        error: function(){
+			console.log("unable to obtain team members");
+        },
+        success: function(data){
+			$.each(data.d.Data.Items, function(){});
+        }
+    });
+	
+}
+// *****************************************************************************
+function inc_loadFilter(strFilters, filtername, additionaldata) {
+	objFilter = JSON.parse(strFilters);
+
+	var objSort = null;
+	try {
+		objSort = JSON.parse(JSON.parse(additionaldata).sort); //eg of additionaldata: {sort:[{field:"category",dir:"asc"}], xyz:[{a:"b"},{c:"d"}]}
+	} catch (e) {
+		objSort = null;
+	}
+
+	//$("#incident_grid").data("kendoGrid").dataSource.filter(objFilter);  //!!!
+	queryGridDataSource("incident_grid", objFilter, objSort);
+
+	$.publish('incidents/filterWindow/AddFilterButtons');
+	$.publish('incidents/searchFilter/setSearchFilterTitle', [filtername]);
+	$.publish('incidents/filterWindow/setFilteredListItemColor', ['green']);
+	var addFilterWindow = $("#addFilterWindow");
+	if (addFilterWindow.is(":visible")) addFilterWindow.data("kendoWindow").close();
+	//inc_setListTypeStyle(objFilter.filters);
+}
+//***********************************************************************************
+
 
 function showFilters(){
   if(document.getElementById('filterButtons').style.display == "" || document.getElementById('filterButtons').style.display == "none"){
